65df0f1d93b737861e1ff6eaa8fd7ba5bc6c64d8
Annas-Ahmed <annasahmed1609@gmail.com>
2022-06-04 02:22:11 +0500

    task5 complete

---
+++ 31-34/app.js
@@ -0,0 +1,56 @@
+// question1
+// console.log(new Date())
+
+// question2
+// month=["Jan", "Feb", "Mar", "Apr", " May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+// alert(month[new Date().getMonth()])
+
+// question3
+// day=["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+// alert(day[new Date().getDay()])
+
+// question4
+// day=["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+// var today=day[new Date().getDay()]
+// if (today=="Sun" || today=="Sat"){
+//     alert("It's a funday")
+// }
+// else{
+//     alert("It's not a funday")
+// }
+
+// question5
+// var toadate=new Date().getDate()
+// if(toadate<=15){
+//     console.log("First Fifteen days of Month")
+// }
+// else{
+//     console.log("Last Fifteen days of Month")
+// }
+
+// question6
+// var toadate = new Date()
+// console.log(toadate.getMilliseconds())
+// console.log(toadate.getMinutes())
+
+// question7
+// var toadate=new Date()
+// // if (date)
+// console.log(toadate.getHours())
+// if (toadate.getHours()>=12){
+//     console.log("its pm")
+// }
+// else{
+//     console.log("its am")
+// }
+
+// question8
+// var todate=new Date()
+// todate.setFullYear(2022,11,31)
+// console.log(todate)
+
+// question9
+// date1=new Date(2022, 03, 23)
+// date2=new Date(2022, 05, 25)
+// diff=date2.getTime()- date1.getTime()
+// console.log( diff / (1000 * 3600 * 24))
